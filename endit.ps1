ad819db851078d0f473c410cd7ffa9079d987447
Nina Reynolds <12169942+cutecycle@users.noreply.github.com>
2021-02-05 00:27:33 -0500

    Create endit.ps1

---
+++ endit.ps1
@@ -0,0 +1,9 @@
+# just close everything
+$exceptions="Teams"
+
+function endit {
+    $parent = (gwmi win32_process | ? processid -eq  $PID).parentprocessid
+    $procs = (Get-Process | Where-Object { $_.MainWindowTitle -ne "" } | Where-Object { $_.Pid -ne $PID } | Where-Object { $_.Id -ne $parent }| Where-Object {$_.ProcessName -NotIn $exceptions})
+    $procs
+    Stop-Process $procs -ErrorAction SilentlyContinue
+}
