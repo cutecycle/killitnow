2d9e64c4425f51fe1ab065fa8794f75d8c203e15
Nina Reynolds <12169942+cutecycle@users.noreply.github.com>
2021-02-24 18:46:26 -0500

    Update endit.ps1

--- endit.ps1
+++ endit.ps1
@@ -1,9 +1,16 @@
-# just close everything
-$exceptions="Teams","powershell"
-
 function endit {
     $parent = (gwmi win32_process | ? processid -eq  $PID).parentprocessid
-    $procs = (Get-Process | Where-Object { $_.MainWindowTitle -ne "" } | Where-Object { $_.Pid -ne $PID } | Where-Object { $_.Id -ne $parent }| Where-Object {$_.ProcessName -NotIn $exceptions})
+    $procs = (Get-Process | Where-Object { $_.MainWindowTitle -ne "" } | Where-Object { $_.Pid -ne $PID } | Where-Object { $_.Id -ne $parent } | Where-Object { $_.ProcessName -NotIn $exceptions })
+    $procs
+    Stop-Process $procs -ErrorAction SilentlyContinue
+}
+function k  {
+    param(
+        [Parameter(Position = 0)]
+        $name
+    )
+    $parent = (gwmi win32_process | ? processid -eq  $PID).parentprocessid
+    $procs = (Get-Process | Where-Object { $_.MainWindowTitle -like "*$name*" -or $_.ProcessName -like "*$name*" } | Where-Object { $_.MainWindowTitle -ne "" } | Where-Object { $_.Pid -ne $PID } | Where-Object { $_.Id -ne $parent } | Where-Object { $_.ProcessName -NotIn $exceptions })
     $procs
     Stop-Process $procs -ErrorAction SilentlyContinue
 }
